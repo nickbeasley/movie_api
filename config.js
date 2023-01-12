00034529a13c1492d391cf2e8d4fdcdd32825928
nickbeasley <109631653+nickbeasley@users.noreply.github.com>
2023-01-11 20:01:54 -0600

    Netlify update

--- config.js
+++ config.js
@@ -2,6 +2,6 @@ const Config = {
   JWT_SECRET: process.env.JWT_SECRET,
   CONNECTION_URI: process.env.CONNECTION_URI,
   API_ROOT: "/.netlify/functions/server",
-  LOCAL_PORT: 8080,
+  LOCAL_PORT: 3000,
 };
 module.exports = Config;
--- server-local.js
+++ server-local.js
@@ -3,5 +3,5 @@ const app = require("./express/server");
 const { LOCAL_PORT } = require("./config");
 
 app.listen(LOCAL_PORT, () =>
-  console.log(`Local app listening on ${LOCAL_PORT}`)
+  console.log(`Local app listening on port ${LOCAL_PORT}!`)
 );
